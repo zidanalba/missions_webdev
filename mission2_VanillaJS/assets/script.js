bd009aa525bce4fc200f49e30c707faa7dc433eb
zidanalba <zmuhammadzidan@gmail.com>
2022-09-06 23:20:27 +0700

    update

--- mission2_VanillaJS/assets/script.js
+++ mission2_VanillaJS/assets/script.js
@@ -1,11 +1,11 @@
-const slider = document.querySelector(".blackSlider");
 const logo = document.getElementById("logo");
+const pPict = document.getElementsByClassName("pPicture");
 
 function hero() {
   const hero = document.querySelector(".hero");
   let heroHeight = 0;
   let heroWidth = 100;
-  heroInterval = setInterval(() => {
+  var heroInterval = setInterval(() => {
     if (heroHeight == 75) {
       clearInterval(heroInterval);
     } else {
@@ -14,7 +14,7 @@ function hero() {
     }
   }, 5);
 
-  heroInterval2 = setInterval(() => {
+  var heroInterval2 = setInterval(() => {
     if (heroHeight == 100) {
       clearInterval(heroInterval2);
     } else {
@@ -23,7 +23,7 @@ function hero() {
     }
   }, 15);
   setTimeout(() => {
-    heroWidthInterval = setInterval(() => {
+    var heroWidthInterval = setInterval(() => {
       if (heroWidth == 90) {
         clearInterval(heroWidthInterval);
       } else {
@@ -31,8 +31,8 @@ function hero() {
         hero.style.width = heroWidth + "%";
       }
     }, 15);
-    heroWidthInterval2 = setInterval(() => {
-      if (heroWidth == 85) {
+    var heroWidthInterval2 = setInterval(() => {
+      if (heroWidth == 75) {
         clearInterval(heroWidthInterval2);
       } else {
         heroWidth--;
@@ -42,8 +42,50 @@ function hero() {
   }, 1000);
 }
 
+function slider() {
+  const slider = document.querySelector(".blackSlider");
+
+  let sliderXPos = 0;
+
+  var sliderWidthInterval = setInterval(() => {
+    if (sliderXPos == 50) {
+      clearInterval(sliderWidthInterval);
+    } else {
+      sliderXPos++;
+      slider.style.width = sliderXPos + "%";
+    }
+  }, 25);
+
+  var sliderWidthIntervali = setInterval(() => {
+    if (sliderXPos == 100) {
+      clearInterval(sliderWidthIntervali);
+    } else {
+      sliderXPos++;
+      slider.style.width = sliderXPos + "%";
+    }
+  }, 35);
+}
+
+function profileFadeIn() {
+  const pPict = document.getElementsByClassName("pPicture");
+  let op = 0;
+
+  fadeIn = setInterval(() => {
+    if (op == 1) {
+      clearInterval(fadeIn);
+    } else {
+      op++;
+      pPict.style.opacity = op;
+    }
+  }, 25);
+}
+
 function landing() {
   hero();
+
+  setTimeout(slider, 1200);
+
+  profileFadeIn();
 }
 
 window.onload = landing();
